76035ebbe1b0224bae9c51eb13a73640a5b14444
gayathri265 <gayathrikumar2605@gmail.com>
2024-10-10 20:18:32 +0530

    Add files via upload
    task 1

---
+++ gayu.py
@@ -0,0 +1,10 @@
+var1 = int(input("enter number 1 to 9:"))
+var2 = int(input("enter number 1to 9:"))
+for i in range(0,10):
+    
+    if i == var1:
+        continue
+    if i == var2:
+        continue 
+    else:
+        print(i)
